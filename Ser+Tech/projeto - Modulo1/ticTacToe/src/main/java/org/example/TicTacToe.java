7ccda7e963e475e8263ba7fcbb0a44e9619b207c
Yasmin <prof.yasminserra@gmail.com>
2026-03-28 10:45:03 -0300

    initial commit

---
+++ Ser+Tech/projeto - Modulo1/ticTacToe/src/main/java/org/example/TicTacToe.java
@@ -0,0 +1,10 @@
+package org.example;
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        int [][] tabuleiro  = new int [3][3];
+
+    }
+
+}
