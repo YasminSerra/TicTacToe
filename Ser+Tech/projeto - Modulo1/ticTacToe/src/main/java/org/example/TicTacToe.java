8726466aadc3bb7c984abb47de860dc0802eeba9
Yasmin <prof.yasminserra@gmail.com>
2026-03-28 12:44:18 -0300

    add turns and simple finalization logic

--- Ser+Tech/projeto - Modulo1/ticTacToe/src/main/java/org/example/TicTacToe.java
+++ Ser+Tech/projeto - Modulo1/ticTacToe/src/main/java/org/example/TicTacToe.java
@@ -6,22 +6,29 @@ public class TicTacToe {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[][] tabuleiro = new String[3][3];
+
+        int turno = 1;
+        System.out.println("###Tic Tac Toe###");
+        System.out.println("Jogador 1 - escolha X ou O:");
+        String jogador1 = sc.next().toUpperCase();
+        String jogador2 = jogador1.equalsIgnoreCase("x")  ? "O" : "X"  ;
+
         while(true) {
-            System.out.println("###Tic Tac Toe###");
-            System.out.print("Digite uma linha (0 a 2) e pressione enter: ");
+            String jogadorAtual = turno == 1 ? "Jogador 1" : "Jogador 2";
+            System.out.print(jogadorAtual + ": digite uma linha (0 a 2) e pressione enter: ");
             int linha = sc.nextInt();
             if (linha > 2 || linha < 0){
                 System.out.println("Valor inválido!");
                 continue;
             }
-            System.out.print("Digite uma coluna (0 a 2) e pressione enter: ");
+            System.out.print(jogadorAtual + ": digite uma coluna (0 a 2) e pressione enter: ");
             int coluna = sc.nextInt();
             if (coluna > 2 || coluna < 0){
                 System.out.println("Valor inválido!");
                 continue;
             }
 
-            tabuleiro[linha][coluna] = "X";
+            tabuleiro[linha][coluna] = turno == 1 ? jogador1 : jogador2;
 
             for (int i = 0; i < 3; i++){
                 for (int j = 0; j < 3; j++) {
@@ -31,13 +38,29 @@ public class TicTacToe {
                 System.out.print("|\n");
             }
 
-            System.out.println("Deseja continuar jogando? 1 =  Sim, 2 = Não");
-            int continuar = sc.nextInt();
+           if (jogoFinalizado(tabuleiro)){
+               System.out.println("Deseja continuar jogando? 1 =  Sim, 2 = Não");
+               int continuar = sc.nextInt();
 
-            if (continuar == 2) {
-                break;
-            }
+               if (continuar == 2) {
+                   break;
+               }
+           }
+
+            turno = turno == 1 ? 2 : 1;
         }
+    }
+
+    public static boolean jogoFinalizado(String[][] tabuleiro){
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++) {
+               if (tabuleiro[i][j] == null){
+                   return false;
+               }
+            }
+
+        }
+         return true;
     }
 
 }
