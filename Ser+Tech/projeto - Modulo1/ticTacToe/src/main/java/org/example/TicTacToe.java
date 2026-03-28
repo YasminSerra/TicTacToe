829017fe5a01082dd44c36af57f8e69f1a6f4fd8
Yasmin <prof.yasminserra@gmail.com>
2026-03-28 11:31:25 -0300

    add game loop

--- Ser+Tech/projeto - Modulo1/ticTacToe/src/main/java/org/example/TicTacToe.java
+++ Ser+Tech/projeto - Modulo1/ticTacToe/src/main/java/org/example/TicTacToe.java
@@ -5,14 +5,25 @@ import java.util.Scanner;
 public class TicTacToe {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("###Tic Tac Toe###");
-        System.out.print("Digite uma linha (0 a 2) e pressione enter: ");
-        int linha = sc.nextInt();
-        System.out.print("Digite uma coluna (0 a 2) e pressione enter: ");
-        int coluna = sc.nextInt();
+        while(true) {
+            System.out.println("###Tic Tac Toe###");
+            System.out.print("Digite uma linha (0 a 2) e pressione enter: ");
+            int linha = sc.nextInt();
+            System.out.print("Digite uma coluna (0 a 2) e pressione enter: ");
+            int coluna = sc.nextInt();
 
-        System.out.println("A linha é: " + linha);
-        System.out.println("A coluna é: " + coluna);
+            System.out.println("A linha é: " + linha);
+            System.out.println("A coluna é: " + coluna);
+
+            System.out.println("Deseja continuar jogando? 1 =  Sim, 2 = Não");
+            int continuar = sc.nextInt();
+
+            if (continuar == 2) {
+                break;
+            }
+
+
+        }
     }
 
 }
