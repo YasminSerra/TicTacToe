59ccbae9b3321c9222371a50df257f2b40e7210a
Yasmin <prof.yasminserra@gmail.com>
2026-03-28 11:15:28 -0300

    add initial user inputs

--- Ser+Tech/projeto - Modulo1/ticTacToe/src/main/java/org/example/TicTacToe.java
+++ Ser+Tech/projeto - Modulo1/ticTacToe/src/main/java/org/example/TicTacToe.java
@@ -1,10 +1,18 @@
 package org.example;
+
 import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args) {
-        int [][] tabuleiro  = new int [3][3];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("###Tic Tac Toe###");
+        System.out.print("Digite uma linha (0 a 2) e pressione enter: ");
+        int linha = sc.nextInt();
+        System.out.print("Digite uma coluna (0 a 2) e pressione enter: ");
+        int coluna = sc.nextInt();
 
+        System.out.println("A linha é: " + linha);
+        System.out.println("A coluna é: " + coluna);
     }
 
 }
